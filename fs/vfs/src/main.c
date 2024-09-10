c461e34a30a132205c50ca02a1371fc1108ae5ed
jewelcodes <omarmelghoul01@gmail.com>
2024-09-10 12:49:18 -0400

    vfs: stub for main

---
+++ fs/vfs/src/main.c
@@ -0,0 +1,10 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * vfs: Microkernel implementing a virtual file system
+ */
+
+int main(int argc, char **argv) {
+    while(1);   // todo
+}
