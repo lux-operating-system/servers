3fe1a08bedc77c4cb67d7053fe42ee6c362c6b32
jewelcodes <omarmelghoul01@gmail.com>
2024-09-10 19:47:25 -0400

    vfs: init and show signs of life

--- fs/vfs/src/main.c
+++ fs/vfs/src/main.c
@@ -2,9 +2,16 @@
  * luxOS - a unix-like operating system
  * Omar Elghoul, 2024
  * 
- * vfs: Microkernel implementing a virtual file system
+ * vfs: Microkernel server implementing a virtual file system
  */
 
+#include <liblux/liblux.h>
+
 int main(int argc, char **argv) {
+    luxInit("vfs");     // this will connect to lux and lumen
+
+    // show signs of life
+    luxLog(KPRINT_LEVEL_DEBUG, "virtual file system server started\n");
+
     while(1);   // todo
 }
