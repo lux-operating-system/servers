67a4b5cadadd28bc09833c45f912dcb239a20072
jewelcodes <omarmelghoul01@gmail.com>
2024-09-13 13:33:41 -0400

    devfs: mount()

---
+++ fs/devfs/src/mount.c
@@ -0,0 +1,28 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * devfs: Microkernel server implementing the /dev file system
+ */
+
+#include <string.h>
+#include <liblux/liblux.h>
+#include <vfs.h>
+#include <devfs/devfs.h>
+
+/* devfsMount(): mounts the devfs file system
+ * params: req - request buffer
+ * params: res - response buffer
+ * returns: nothing
+ */
+
+void devfsMount(SyscallHeader *req, SyscallHeader *res) {
+    MountCommand *cmd = (MountCommand *) req;
+    luxLogf(KPRINT_LEVEL_DEBUG, "mounting devfs at %s\n", cmd->target);
+
+    memcpy(res, req, sizeof(SyscallHeader));
+    res->header.response = 1;
+    res->header.status = 0;     // success
+    res->header.length = sizeof(SyscallHeader);
+    luxSendDependency(res);
+}
