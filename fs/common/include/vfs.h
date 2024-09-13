ccaa94aa5c3dbc30cacc79ec403797f165074f81
jewelcodes <omarmelghoul01@gmail.com>
2024-09-12 22:16:50 -0400

    vfs: common include file for all file systems

---
+++ fs/common/include/vfs.h
@@ -0,0 +1,17 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * vfs: Message structures for file system servers that depend on the VFS
+ */
+
+#pragma once
+
+#include <liblux/liblux.h>
+
+#define COMMAND_VFS_INIT            0xFFFF
+
+typedef struct {
+    MessageHeader header;
+    char fsType[16];
+} VFSInitCommand;
