a8dd58289b32d5f9ca01a3146a1918d4ac36cd65
jewelcodes <omarmelghoul01@gmail.com>
2024-09-13 12:05:23 -0400

    vfs: track loaded file system drivers

--- fs/vfs/src/main.c
+++ fs/vfs/src/main.c
@@ -9,6 +9,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <liblux/liblux.h>
+#include <vfs.h>
+
+FileSystemServers *servers;
+int serverCount = 0;
 
 int main(int argc, char **argv) {
     luxInit("vfs");     // this will connect to lux and lumen
@@ -17,14 +21,15 @@ int main(int argc, char **argv) {
     luxLogf(KPRINT_LEVEL_DEBUG, "virtual file system server started with pid %d\n", getpid());
 
     SyscallHeader *req = calloc(1, SERVER_MAX_SIZE);
-    SyscallHeader *res = calloc(1, SERVER_MAX_SIZE);
+    servers = calloc(MAX_FILE_SYSTEMS, sizeof(FileSystemServers));
 
-    if(!req || !res) {
-        luxLog(KPRINT_LEVEL_ERROR, "unable to allocate memory for vfs messages\n");
-        while(1) sched_yield();
+    if(!req || !servers) {
+        luxLog(KPRINT_LEVEL_ERROR, "unable to allocate memory for vfs\n");
+        return -1;
     }
 
     ssize_t s;
+    int sd;
     while(1) {
         // wait for incoming requests
         s = luxRecvLumen(req, SERVER_MAX_SIZE, false);
@@ -35,6 +40,30 @@ int main(int argc, char **argv) {
             req->header.response = 1;
             req->header.status = 0;     // "success"
             luxSendLumen(req);
+        }
+
+        // and also accept incoming connections
+        sd = luxAccept();
+        if(sd > 0) {
+            // append to the list
+            servers[serverCount].socket = sd;
+            serverCount++;
+        }
+
+        // and handle messages from dependent servers
+        if(serverCount) {
+            for(int i = 0; i < serverCount; i++) {
+                s = luxRecv(servers[i].socket, req, SERVER_MAX_SIZE, false);
+                if(s > 0 && s <= SERVER_MAX_SIZE) {
+                    if(req->header.command == COMMAND_VFS_INIT) {   // special command
+                        VFSInitCommand *init = (VFSInitCommand *)req;
+                        strcpy(servers[i].type, init->fsType);
+                        luxLogf(KPRINT_LEVEL_DEBUG, "loaded file system driver for '%s'\n", servers[i].type);
+                    } else {
+                        luxLogf(KPRINT_LEVEL_WARNING, "unimplemented command 0x%X from file system driver for '%s'\n", req->header.command, servers[i].type);
+                    }
+                }
+            }
         }
     }
 }
