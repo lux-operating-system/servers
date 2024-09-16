fbe813cb58c179d9ca8a8139dbb3b5436c2b8090
jewelcodes <omarmelghoul01@gmail.com>
2024-09-15 21:17:13 -0400

    devfs: device file structure

--- fs/devfs/src/include/devfs/devfs.h
+++ fs/devfs/src/include/devfs/devfs.h
@@ -8,5 +8,19 @@
 #pragma once
 
 #include <liblux/liblux.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* generic structure that will be used to maintain a list of files on /dev */
+
+typedef struct {
+    char name[MAX_FILE_PATH];
+    struct stat status;
+    void (*ioHandler)(int, off_t, size_t);
+} DeviceFile;
 
 extern void (*dispatchTable[])(SyscallHeader *, SyscallHeader *);
+extern DeviceFile *devices;
+extern int deviceCount;
+
+void createDevice(char *, void (*)(int, off_t, size_t), struct stat *);
