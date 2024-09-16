073ccdd565d2fd8aad120b5477a8b036408150e5
jewelcodes <omarmelghoul01@gmail.com>
2024-09-15 22:10:48 -0400

    devfs: implemented stat()

--- fs/devfs/src/stat.c
+++ fs/devfs/src/stat.c
@@ -6,6 +6,7 @@
  */
 
 #include <string.h>
+#include <errno.h>
 #include <liblux/liblux.h>
 #include <vfs.h>
 #include <devfs/devfs.h>
@@ -13,10 +14,23 @@
 /* devfsStat(): returns the file status of a file on the /dev file system
  * params: req - request buffer
  * params: res - response buffer
- * returns: nothing
+ * returns: nothing, response forwarded to vfs server
  */
 
 void devfsStat(SyscallHeader *req, SyscallHeader *res) {
     StatCommand *cmd = (StatCommand *) req;
-    luxLogf(KPRINT_LEVEL_DEBUG, "stat for %s\n", cmd->path);
+    memcpy(res, req, req->header.length);
+
+    res->header.response = 1;
+
+    DeviceFile *dev = findDevice(cmd->path);
+    if(!dev) {
+        res->header.status = -ENOENT;   // file doesn't exist
+    } else {
+        StatCommand *response = (StatCommand *) res;
+        response->header.header.status = 0;
+        memcpy(&response->buffer, &dev->status, sizeof(struct stat));
+    }
+
+    luxSendDependency(res);
 }
