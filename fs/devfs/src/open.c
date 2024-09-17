8aad412f90a685885b2cbe0fb1d3208e884cb580
jewelcodes <omarmelghoul01@gmail.com>
2024-09-17 16:12:29 -0400

    devfs: check file permissions for open()

--- fs/devfs/src/open.c
+++ fs/devfs/src/open.c
@@ -5,7 +5,9 @@
  * devfs: Microkernel server implementing the /dev file system
  */
 
+#include <errno.h>
 #include <string.h>
+#include <fnctl.h>
 #include <liblux/liblux.h>
 #include <vfs.h>
 #include <devfs/devfs.h>
@@ -13,5 +15,30 @@
 void devfsOpen(SyscallHeader *req, SyscallHeader *res) {
     OpenCommand *cmd = (OpenCommand *) req;
     memcpy(res, req, req->header.length);
-    luxLogf(KPRINT_LEVEL_DEBUG, "opening file '%s'\n", cmd->path);
+    res->header.response = 1;
+
+    DeviceFile *file = findDevice(cmd->path);
+    if(!file) {
+        res->header.status = -ENOENT;   // file doesn't exist
+        luxSendDependency(res);
+    }
+
+    // make sure the requester has the appropriate permissions
+    res->header.status = 0;     // "success"
+
+    if(cmd->uid == file->status.st_uid) {
+        // owner
+        if(!((cmd->flags & O_RDONLY) && (file->status.st_mode & S_IRUSR))) res->header.status = -EACCES;
+        if(!((cmd->flags & O_WRONLY) && (file->status.st_mode & S_IWUSR))) res->header.status = -EACCES;
+    } else if(cmd->gid == file->status.st_gid) {
+        // group
+        if(!((cmd->flags & O_RDONLY) && (file->status.st_mode & S_IRGRP))) res->header.status = -EACCES;
+        if(!((cmd->flags & O_WRONLY) && (file->status.st_mode & S_IWGRP))) res->header.status = -EACCES;
+    } else {
+        // other
+        if(!((cmd->flags & O_RDONLY) && (file->status.st_mode & S_IROTH))) res->header.status = -EACCES;
+        if(!((cmd->flags & O_WRONLY) && (file->status.st_mode & S_IWOTH))) res->header.status = -EACCES;
+    }
+
+    luxSendDependency(res);
 }
