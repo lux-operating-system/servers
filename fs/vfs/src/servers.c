33d30fdcbd85ce14fd67ddbaee991ca3b9a036c8
jewelcodes <omarmelghoul01@gmail.com>
2024-09-17 19:02:49 -0400

    vfs: find file system driver by mountpoint

--- fs/vfs/src/servers.c
+++ fs/vfs/src/servers.c
@@ -24,3 +24,18 @@ int findFSServer(const char *type) {
 
     return -1;
 }
+
+/* findMountpoint(): returns the socket descriptor of a fs driver from a monutpoint
+ * params: mp - mounted device path
+ * returns: socket descriptor, -1 if no driver is loaded or no such mountpoint
+ */
+
+int findMountpoint(const char *mp) {
+    if(!serverCount) return -1;
+
+    for(int i = 0; i < serverCount; i++) {
+        if(!strcmp(mp, mps[i].device)) return findFSServer(mps[i].type);
+    }
+
+    return -1;
+}
