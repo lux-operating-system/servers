18d7b481ce7085304bec314d73190a2b177267b6
jewelcodes <omarmelghoul01@gmail.com>
2024-09-20 18:10:14 -0400

    devfs: initialize external driver subsystem

---
+++ fs/devfs/src/drivers.c
@@ -0,0 +1,44 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * devfs: Microkernel server implementing the /dev file system
+ */
+
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <liblux/liblux.h>
+#include <liblux/devfs.h>
+#include <devfs/devfs.h>
+
+static int *connections;
+static struct sockaddr *servers;
+static struct socklen_t *addrlens;
+static int count;
+
+/* driverInit(): initializes the driver subsystem
+ * params: none
+ * returns: nothing
+ */
+
+void driverInit() {
+    connections = calloc(MAX_DRIVERS, sizeof(int));
+    servers = calloc(MAX_DRIVERS, sizeof(struct sockaddr));
+    addrlens = calloc(MAX_DRIVERS, sizeof(socklen_t));
+
+    if(!connections || !servers || !addrlens) {
+        luxLogf(KPRINT_LEVEL_ERROR, "failed to allocate memory for driver subsystem\n");
+        exit(-1);
+    }
+
+    count = 0;
+}
+
+/* driverHandle(): handles incoming requests from drivers
+ * params: none
+ * returns: nothing
+ */
+
+void driverHandle() {
+    // TODO
+}
