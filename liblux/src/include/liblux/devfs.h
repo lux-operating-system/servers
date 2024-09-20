09a1662d8ad42f792d51e5587305baf03637ecd0
jewelcodes <omarmelghoul01@gmail.com>
2024-09-20 15:50:26 -0400

    liblux: structures for registering device on /dev

---
+++ liblux/src/include/liblux/devfs.h
@@ -0,0 +1,21 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * liblux: Library abstracting kernel-server communication protocols
+ */
+
+#pragma once
+
+#include <liblux/liblux.h>
+
+/* These commands are requested by device drivers from devfs */
+#define COMMAND_DEVFS_REGISTER          0xD000  /* register a device on /dev */
+#define COMMAND_DEVFS_UNREGISTER        0xD001  /* unregister a device */
+#define COMMAND_DEVFS_STATUS            0xD002  /* device status */
+
+typedef struct {
+    MessageHeader header;
+    char path[MAX_FILE_PATH];           // path on /dev
+    char server[256];                   // driver to handle the request
+} DevfsRegisterCommand;
