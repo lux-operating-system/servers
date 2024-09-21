71244e5d5d23b61121fe225f4abd750fc0921758
jewelcodes <omarmelghoul01@gmail.com>
2024-09-21 16:56:54 -0400

    lfb: create frame buffer device under /dev/lfb

--- devices/lfb/src/main.c
+++ devices/lfb/src/main.c
@@ -5,6 +5,47 @@
  * lfb: Abstraction for linear frame buffers under /dev/lfb
  */
 
+#include <liblux/liblux.h>
+#include <liblux/devfs.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+
+static FramebufferResponse fb;
+
 int main() {
-    return 0;
+    luxInit("lfb");
+    while(luxConnectDependency("devfs"));   // depend on /dev
+
+    // request the frame buffer from the kernel
+    if(luxRequestFramebuffer(&fb)) {
+        luxLogf(KPRINT_LEVEL_ERROR, "failed to acquire from kernel\n");
+        return -1;
+    }
+
+    luxLogf(KPRINT_LEVEL_DEBUG, "screen resolution is %dx%d (%d bpp)\n", fb.w, fb.h, fb.bpp);
+
+    // create a character device on /dev for the frame buffer
+    DevfsRegisterCommand *regcmd = calloc(1, sizeof(DevfsRegisterCommand));
+    struct stat *status = calloc(1, sizeof(struct stat));
+    if(!regcmd || !status) {
+        luxLogf(KPRINT_LEVEL_ERROR, "failed to allocate memory for frame buffer device\n");
+        return -1;
+    }
+
+    // character device with permissions rw-rw-r--
+    status->st_mode = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IFCHR);
+    status->st_size = fb.w * fb.h * fb.bpp / 8;
+
+    // register the device
+    regcmd->header.command = COMMAND_DEVFS_REGISTER;
+    regcmd->header.length = sizeof(DevfsRegisterCommand);
+    strcpy(regcmd->path, "/lfb0");
+    strcpy(regcmd->server, "lux:///dslfb");  // server name prefixed with "lux:///ds"
+    memcpy(&regcmd->status, status, sizeof(struct stat));
+    luxSendDependency(regcmd);
+
+    // notify lumen that startup is complete
+    luxReady();
+    for(;;);
 }
