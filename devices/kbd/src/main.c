b76865f33d9d93d7caa109ebf95e718342d82644
jewelcodes <omarmelghoul01@gmail.com>
2024-09-20 21:15:43 -0400

    kbd: request creation of /dev/kbd

--- devices/kbd/src/main.c
+++ devices/kbd/src/main.c
@@ -5,14 +5,33 @@
  * kbd: Abstraction for keyboard devices under /dev/kbd
  */
 
+#include <string.h>
+#include <sys/stat.h>
 #include <liblux/liblux.h>
+#include <liblux/devfs.h>
 
 int main() {
     luxInit("kbd");
     while(luxConnectDependency("devfs"));
 
-    luxLogf(KPRINT_LEVEL_DEBUG, "sign of life\n");
+    // create a keyboard device under /dev
+    struct stat status;
+    memset(&status, 0, sizeof(struct stat));
+    status.st_size = 4096;
 
+    // character device, r--r--r--
+    status.st_mode = (S_IRUSR | S_IRGRP | S_IROTH | S_IFCHR);
+
+    DevfsRegisterCommand regcmd;
+    memset(&regcmd, 0, sizeof(DevfsRegisterCommand));
+    regcmd.header.command = COMMAND_DEVFS_REGISTER;
+    regcmd.header.length = sizeof(DevfsRegisterCommand);
+    strcpy(regcmd.path, "/kbd");
+    strcpy(regcmd.server, "lux:///dskbd");  // server name prefixed with "lux:///ds"
+    memcpy(&regcmd.status, &status, sizeof(struct stat));
+    luxSendDependency(&regcmd);
+
+    // notify lumen that startup is complete
     luxReady();
     while(1);
 }
