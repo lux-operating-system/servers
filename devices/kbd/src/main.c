2c947f60cd8df4f69539427673cb26cc18595cd9
jewelcodes <omarmelghoul01@gmail.com>
2024-09-20 21:43:06 -0400

    kbd: refactor to not use local var space for everything

--- devices/kbd/src/main.c
+++ devices/kbd/src/main.c
@@ -6,6 +6,7 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <liblux/liblux.h>
 #include <liblux/devfs.h>
@@ -15,21 +16,28 @@ int main() {
     while(luxConnectDependency("devfs"));
 
     // create a keyboard device under /dev
-    struct stat status;
-    memset(&status, 0, sizeof(struct stat));
-    status.st_size = 4096;
+    struct stat *status = calloc(1, sizeof(struct stat));
+    DevfsRegisterCommand *regcmd = calloc(1, sizeof(DevfsRegisterCommand));
+
+    if(!status || !regcmd) {
+        luxLogf(KPRINT_LEVEL_ERROR, "unable to allocate memory for keyboard server\n");
+        return -1;
+    }
 
     // character device, r--r--r--
-    status.st_mode = (S_IRUSR | S_IRGRP | S_IROTH | S_IFCHR);
+    status->st_mode = (S_IRUSR | S_IRGRP | S_IROTH | S_IFCHR);
+    status->st_size = 4096;
 
-    DevfsRegisterCommand regcmd;
-    memset(&regcmd, 0, sizeof(DevfsRegisterCommand));
-    regcmd.header.command = COMMAND_DEVFS_REGISTER;
-    regcmd.header.length = sizeof(DevfsRegisterCommand);
-    strcpy(regcmd.path, "/kbd");
-    strcpy(regcmd.server, "lux:///dskbd");  // server name prefixed with "lux:///ds"
-    memcpy(&regcmd.status, &status, sizeof(struct stat));
-    luxSendDependency(&regcmd);
+    // and register the device
+    regcmd->header.command = COMMAND_DEVFS_REGISTER;
+    regcmd->header.length = sizeof(DevfsRegisterCommand);
+    strcpy(regcmd->path, "/kbd");
+    strcpy(regcmd->server, "lux:///dskbd");  // server name prefixed with "lux:///ds"
+    memcpy(&regcmd->status, status, sizeof(struct stat));
+    luxSendDependency(regcmd);
+
+    free(status);
+    free(regcmd);
 
     // notify lumen that startup is complete
     luxReady();
