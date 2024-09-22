8fb0b24130d4594e942906b1a5b1376ec2183acb
jewelcodes <omarmelghoul01@gmail.com>
2024-09-22 18:00:41 -0400

    vfs: relay ioctl() to devfs server

--- fs/vfs/src/dispatch.c
+++ fs/vfs/src/dispatch.c
@@ -8,6 +8,8 @@
 #include <liblux/liblux.h>
 #include <vfs.h>
 #include <vfs/vfs.h>
+#include <errno.h>
+#include <string.h>
 
 void vfsDispatchMount(SyscallHeader *hdr) {
     MountCommand *cmd = (MountCommand *) hdr;
@@ -66,6 +68,27 @@ void vfsDispatchWrite(SyscallHeader *hdr) {
     }
 }
 
+void vfsDispatchIoctl(SyscallHeader *hdr) {
+    IOCTLCommand *cmd = (IOCTLCommand *) hdr;
+    char type[32];
+    if(resolve(cmd->path, type, cmd->device, cmd->path)) {
+        // ioctl() is only valid for /dev because it manipulates device files
+        if(strcmp(type, "devfs")) {
+            cmd->header.header.length = sizeof(IOCTLCommand);
+            cmd->header.header.response = 1;
+            cmd->header.header.status = -ENOTTY;
+            luxSendLumen(cmd);
+            return;
+        }
+
+        int sd = findFSServer(type);
+        if(sd <= 0) luxLogf(KPRINT_LEVEL_WARNING, "no file system driver loaded for '%s'\n", type);
+        else luxSend(sd, cmd);
+    } else {
+        luxLogf(KPRINT_LEVEL_WARNING, "could not resolve path '%s'\n", cmd->path);
+    }
+}
+
 void (*vfsDispatchTable[])(SyscallHeader *) = {
     vfsDispatchStat,    // 0 - stat()
     NULL,               // 1 - flush()
@@ -74,4 +97,5 @@ void (*vfsDispatchTable[])(SyscallHeader *) = {
     vfsDispatchOpen,    // 4 - open()
     vfsDispatchRead,    // 5 - read()
     vfsDispatchWrite,   // 6 - write()
+    vfsDispatchIoctl,   // 7 - ioctl()
 };
