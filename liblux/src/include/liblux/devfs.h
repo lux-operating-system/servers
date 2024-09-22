7e9dcf1ddb3126b95fd60a6d80b6bfa04e7e5969
jewelcodes <omarmelghoul01@gmail.com>
2024-09-22 19:33:27 -0400

    liblux: added devfs message to change stat

--- liblux/src/include/liblux/devfs.h
+++ liblux/src/include/liblux/devfs.h
@@ -14,9 +14,10 @@
 #define COMMAND_DEVFS_REGISTER          0xD000  /* register a device on /dev */
 #define COMMAND_DEVFS_UNREGISTER        0xD001  /* unregister a device */
 #define COMMAND_DEVFS_STATUS            0xD002  /* device status */
+#define COMMAND_DEVFS_CHSTAT            0xD003  /* change stat structure */
 
 #define COMMAND_MIN_DEVFS               0xD000
-#define COMMAND_MAX_DEVFS               0xD002
+#define COMMAND_MAX_DEVFS               0xD003
 
 typedef struct {
     MessageHeader header;
@@ -25,3 +26,9 @@ typedef struct {
     struct stat status;
     int handleOpen;     // set to 1 if the driver will handle open()/close()
 } DevfsRegisterCommand;
+
+typedef struct {
+    MessageHeader header;
+    char path[MAX_FILE_PATH];
+    struct stat status;
+} DevfsChstatCommand;
