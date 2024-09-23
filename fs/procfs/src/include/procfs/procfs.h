6368acc2f0c671e9849b5ebe2d4b58032fd5e388
jewelcodes <omarmelghoul01@gmail.com>
2024-09-23 17:54:07 -0400

    procfs: resolve paths

--- fs/procfs/src/include/procfs/procfs.h
+++ fs/procfs/src/include/procfs/procfs.h
@@ -8,5 +8,33 @@
 #pragma once
 
 #include <liblux/liblux.h>
+#include <sys/types.h>
+
+/* for /proc/kernel, /proc/memsize, /proc/memusage, etc */
+#define RESOLVE_KERNEL              1
+#define RESOLVE_MEMSIZE             2
+#define RESOLVE_MEMUSAGE            3
+#define RESOLVE_PAGESIZE            4
+#define RESOLVE_UPTIME              5
+
+/* for /proc/pid/X*/
+#define RESOLVE_PID                 0x8000
+#define RESOLVE_PID_TYPE            (0 | RESOLVE_PID)   /* /proc/pid/type */
+#define RESOLVE_PID_CMD             (1 | RESOLVE_PID)   /* /proc/pid/cmd */
+#define RESOLVE_PID_CWD             (2 | RESOLVE_PID)   /* /proc/pid/cwd */
+#define RESOLVE_PID_MEM             (3 | RESOLVE_PID)   /* /proc/pid/mem */
+#define RESOLVE_PID_IOPORTS         (4 | RESOLVE_PID)   /* /proc/pid/ioports */
+#define RESOLVE_PID_USER            (5 | RESOLVE_PID)   /* /proc/pid/user */
+#define RESOLVE_PID_GROUP           (6 | RESOLVE_PID)   /* /proc/pid/group */
+#define RESOLVE_PID_TIDS            (7 | RESOLVE_PID)   /* /proc/pid/tids */
+#define RESOLVE_PID_PARENT          (8 | RESOLVE_PID)   /* /proc/pid/parent */
+#define RESOLVE_PID_CHILDREN        (9 | RESOLVE_PID)   /* /proc/pid/children */
+#define RESOLVE_PID_STAT            (10 | RESOLVE_PID)  /* /proc/pid/stat */
 
 void procfsMount(MountCommand *, MountCommand *);
+void procfsStat(StatCommand *, StatCommand *);
+void procfsOpen(OpenCommand *, OpenCommand *);
+void procfsRead(RWCommand *, RWCommand *);
+void procfsWrite(RWCommand *, RWCommand *);
+
+int procfsResolve(const char *, pid_t *);
---
+++ fs/procfs/src/resolve.c
@@ -0,0 +1,28 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * procfs: Microkernel server implementing the /proc file system
+ */
+
+#include <procfs/procfs.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* resolve(): resolves a path on the /proc file system
+ * params: path - file name
+ * params: pid - destination to store pid buffer for /proc/pid/X
+ * returns: type of resolved path, -1 if non-existent
+ */
+
+int resolve(const char *path, pid_t *pid) {
+    if(!path) return -1;
+    if(!strcmp(path, "/kernel")) return RESOLVE_KERNEL;
+    if(!strcmp(path, "/memsize")) return RESOLVE_MEMSIZE;
+    if(!strcmp(path, "/memusage")) return RESOLVE_MEMUSAGE;
+    if(!strcmp(path, "/pagesize")) return RESOLVE_PAGESIZE;
+    if(!strcmp(path, "/uptime")) return RESOLVE_UPTIME;
+
+    // TODO
+    return -1;
+}
