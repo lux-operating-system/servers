4168ce46db5a9540fbe2df26d2c5e8a0a9c47ce6
jewelcodes <omarmelghoul01@gmail.com>
2024-09-23 18:22:55 -0400

    devfs: allow creating directory hierarchies

--- fs/devfs/src/device.c
+++ fs/devfs/src/device.c
@@ -8,8 +8,67 @@
 #include <devfs/devfs.h>
 #include <liblux/liblux.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+/* copyPathDepth(): copies a path up to depth n
+ * params: dst - destination buffer
+ * params: path - path
+ * params: n - depth
+ * returns: pointer to destination on success, NULL on error
+ */
+
+char *copyPathDepth(char *dst, const char *path, int n) {
+    int depth = 0;
+    for(int i = 0; i < strlen(path); i++) {
+        if(path[i] == '/') depth++;
+        if(depth > n) return dst;
+
+        dst[i] = path[i];
+    }
+
+    return dst;
+}
+
+/* createDirectories(): creates the directory hierarchy if necessary
+ * params: name - name of the device file
+ * returns: zero on success
+ */
+
+int createDirectories(const char *name) {
+    // first check if this is even necessary at all
+    int depth = 0;
+    for(int i = 0; i < strlen(name); i++) {
+        if(name[i] == '/') depth++;
+    }
+
+    if(depth <= 1) return 0;
+
+    char *dirs = malloc(strlen(name)+1);
+    if(!dirs) return -1;
+
+    DeviceFile *entry;
+
+    // default directory permissions are rwxr-xr-x
+    struct stat dirstat;
+    memset(&dirstat, 0, sizeof(struct stat));
+    dirstat.st_mode = S_IFDIR | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+
+    for(int i = 0; i < depth-1; i++) {
+        copyPathDepth(dirs, name, i+1);
+        entry = findDevice(dirs);
+        if(!entry) {
+            // create the directory if non-existent
+            if(createDevice(dirs, NULL, &dirstat)) return -1;
+        } else {
+            // ensure this is a directory
+            if((entry->status.st_mode & S_IFMT) != S_IFDIR) return -1;
+        }
+    }
+
+    return 0;
+}
 
 /* createDevice(): creates a device file
  * params: name - name of the device file
@@ -20,6 +79,7 @@
 
 int createDevice(const char *name, ssize_t (*handler)(int, const char *, off_t *, void *, size_t), struct stat *status) {
     if(deviceCount >= MAX_DEVICES) return -1;
+    if(createDirectories(name)) return -1;
 
     char *mode = NULL;
     switch(status->st_mode & S_IFMT) {
