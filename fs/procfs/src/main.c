9caa8697c51220473261d2820923f7eb8f2cbb3a
jewelcodes <omarmelghoul01@gmail.com>
2024-09-23 17:08:40 -0400

    procfs: stub for procfs server

---
+++ fs/procfs/src/main.c
@@ -0,0 +1,10 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * procfs: Microkernel server implementing the /proc file system
+ */
+
+int main() {
+    return 0;   // todo
+}
