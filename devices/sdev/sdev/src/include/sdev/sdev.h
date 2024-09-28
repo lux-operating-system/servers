0963e4a1595afba455d2db944c9f5fbb69d07c1e
jewelcodes <omarmelghoul01@gmail.com>
2024-09-28 00:22:21 -0400

    sdev: initialization and basic structure

---
+++ devices/sdev/sdev/src/include/sdev/sdev.h
@@ -0,0 +1,26 @@
+/*
+ * luxOS - a unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * sdev: Abstraction for storage devices under /dev/sdX
+ */
+
+#pragma once
+
+#include <sys/types.h>
+
+typedef struct StorageDevice {
+    struct StorageDevice *next;
+    char name[256];             // name under /dev
+    char server[256];           // server that handles this device
+    uint64_t deviceID;          // driver-specific ID for this device
+    int partition;              // partitioned?
+    uint64_t size;              // sectors
+    uint16_t sectorSize;        // bytes
+
+    int root;                   // set for the root of a partitioned device
+
+    // the remaining fields are not valid if root == 0
+    uint64_t partitionStart;    // sectors
+    uint64_t partitionSize;     // sectors
+} StorageDevice;
--- devices/sdev/sdev/src/main.c
+++ devices/sdev/sdev/src/main.c
@@ -5,6 +5,17 @@
  * sdev: Abstraction for storage devices under /dev/sdX
  */
 
+#include <liblux/liblux.h>
+#include <liblux/sdev.h>
+#include <sdev/sdev.h>
+
 int main() {
-    return 0;   // stub
+    luxInit("sdev");
+    while(luxConnectDependency("devfs"));   // obviously depend on devfs
+
+    // there really isn't anything to do here until storage device drivers are
+    // loaded, so notify lumen that we're ready
+    luxReady();
+
+    while(1);
 }
