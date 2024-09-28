b530e43e3f50621ed57fa50904af2ec81d0c937e
jewelcodes <omarmelghoul01@gmail.com>
2024-09-28 00:11:07 -0400

    nvme: write sectors using NVM I/O commands

--- devices/sdev/nvme/src/nvmio.c
+++ devices/sdev/nvme/src/nvmio.c
@@ -50,3 +50,41 @@ uint16_t nvmeReadSector(NVMEController *drive, int ns, uint16_t id, uint64_t lba
     
     return id;
 }
+
+/* nvmeWriteSector(): writes contiguous sectors from memory to an NVMe SSD
+ * params: drive - drive to write to
+ * params: id - unique non-zero command ID
+ * params: ns - namespace within the drive to write to
+ * params: lba - starting LBA address
+ * params: count - number of sectors to write
+ * params: buffer - buffer to write from
+ * returns: command ID on success, zero on fail
+ */
+
+uint16_t nvmeWriteSector(NVMEController *drive, int ns, uint16_t id, uint64_t lba, uint16_t count, const void *buffer) {
+    // input validation
+    if(ns >= drive->nsCount || !count || !id) return 0;
+    if((lba + count) >= drive->nsSizes[ns]) return 0;
+
+    // we will need the length of the transfer in bytes for the DMA
+    size_t len = count * drive->nsSectorSizes[ns];
+
+    NVMECommonCommand cmd;
+    memset(&cmd, 0, sizeof(NVMECommonCommand));
+    cmd.dword0 = NVM_WRITE | (id << 16);
+    cmd.namespaceID = drive->ns[ns];
+    cmd.metaptr = 0;
+    if(nvmeCreatePRP(drive, &cmd, buffer, len)) return 0;
+
+    cmd.dword2 = 0;
+    cmd.dword3 = 0;
+    cmd.dword10 = lba;
+    cmd.dword11 = (lba >> 32);
+    cmd.dword12 = count-1;
+    cmd.dword13 = 0;
+    cmd.dword14 = 0;
+    cmd.dword15 = 0;
+    nvmeSubmit(drive, 1, &cmd);
+    
+    return id;
+}
