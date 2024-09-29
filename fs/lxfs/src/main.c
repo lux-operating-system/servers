dd6a273ce499d8828fb62de33e26c74a5ea3fb52
jewelcodes <omarmelghoul01@gmail.com>
2024-09-29 11:57:59 -0400

    lxfs: basic startup

--- fs/lxfs/src/main.c
+++ fs/lxfs/src/main.c
@@ -5,6 +5,33 @@
  * lxfs: Driver for the lxfs file system
  */
 
+#include <liblux/liblux.h>
+#include <vfs.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main() {
-    return 0;   // stub
+    luxInit("lxfs");
+    while(luxConnectDependency("vfs"));
+
+    SyscallHeader *msg = calloc(1, SERVER_MAX_SIZE);
+
+    if(!msg) {
+        luxLogf(KPRINT_LEVEL_ERROR, "unable to allocate memory\n");
+        return -1;
+    }
+
+    // notify the vfs that we are a file system driver
+    VFSInitCommand init;
+    memset(&init, 0, sizeof(VFSInitCommand));
+    init.header.command = COMMAND_VFS_INIT;
+    init.header.length = sizeof(VFSInitCommand);
+    init.header.requester = luxGetSelf();
+    strcpy(init.fsType, "lxfs");
+    luxSendDependency(&init);
+
+    // and notify lumen that startup is complete
+    luxReady();
+
+    for(;;);
 }
