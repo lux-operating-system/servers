48139c6cade6d5fb78639aa9d60f8f36c1608823
jewelcodes <omarmelghoul01@gmail.com>
2024-10-03 21:38:33 -0400

    kthd: resolve to absolute path in chdir()

--- kthd/src/chdir.c
+++ kthd/src/chdir.c
@@ -7,7 +7,57 @@
 
 #include <liblux/liblux.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <errno.h>
+
+/* clean(): helper function to clean up a path
+ * params: path - path to be cleaned, the string will be directly modified
+ * params: pointer to path
+ */
+
+static char *clean(char *path) {
+    if(strlen(path) == 1) return path;  // root will never need to be cleaned
+
+    // first try to get rid of excessive slashes
+    for(int i = 0; i < strlen(path); i++) {
+        if((path[i] == '/') && (i < strlen(path)-1) && (path[i+1] == '/')) {
+            memmove(&path[i], &path[i+1], strlen(&path[i])+1);
+            continue;
+        }
+    }
+
+    // if the last character is a slash, remove it except for the root dir
+    if(strlen(path) == 1) return path;
+    while(path[strlen(path)-1] == '/') path[strlen(path)-1] = 0;
+
+    // parse '../' to reflect the parent directory
+    for(int i = 0; i < strlen(path); i++) {
+        if((path[i] == '.') && (path[i+1] == '.') && ((path[i+2] == '/') || (!path[i+2]))) {
+            // find the parent
+            int parent = i-2;
+            for(; parent > 0; parent--) {
+                if(path[parent] == '/') break;
+            }
+
+            parent++;
+            memmove(&path[parent], &path[i+3], strlen(&path[i+3])+1);
+        }
+    }
+
+    // remove './' because it refers to the self directory
+    for(int i = 0; i < strlen(path); i++) {
+        if((path[i] == '.') && ((path[i+1] == '/') || (!path[i+1]))) {
+            memmove(&path[i], &path[i+2], strlen(&path[i+2])+1);
+            continue;
+        }
+    }
+
+    // and finally remove any trailing slashes left while processing the string
+    if(strlen(path) == 1) return path;
+    while(path[strlen(path)-1] == '/') path[strlen(path)-1] = 0;
+
+    return path;
+}
 
 void kthdChdir(ChdirCommand *cmd) {
     cmd->header.header.response = 1;
@@ -37,5 +87,7 @@ void kthdChdir(ChdirCommand *cmd) {
         if(!(st.st_mode & S_IXOTH)) cmd->header.header.status = -EPERM;
     }
 
+    // success, clean up the path
+    clean(cmd->path);
     luxSendLumen(cmd);
 }
